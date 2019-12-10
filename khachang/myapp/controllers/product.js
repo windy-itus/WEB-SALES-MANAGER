a209480bad902214b7f247536ca30ba62a3ff2f3
vxhien <vxhien96@gmail.com>
2019-12-10 20:30:44 +0700

    phân trang, lọc sản phẩm theo nhiều tiêu chí, lựa chọn sắp xếp danh sách

--- khachang/myapp/controllers/product.js
+++ khachang/myapp/controllers/product.js
@@ -1,122 +1,183 @@
-const db = require('../models/product').getDBProduct();
 
+const product = require('../models/product').getProduct;
 
+var categories = [
+  { id: 0, name: "Tất cả" },
+  { id: 1, name: "Điện thoại" },
+  { id: 2, name: "Laptop" },
+  { id: 3, name: "Loa" },
+  { id: 4, name: "Tủ lạnh" },
+  { id: 5, name: "Đồng hồ" },
+  { id: 6, name: "Máy ảnh" }
+];
+var sortOpts = [
+  { id: 0, name: "--" },
+  { id: 1, name: "Tên A-Z" },
+  { id: 2, name: "Tên Z-A" },
+  { id: 3, name: "Giá thấp đến cao" },
+  { id: 4, name: "Giá cao đến thấp" }
+];
+var filter = { selectedCate: 0, selectedSort: 0 }
+const prodPerPage = 8; // product per page
+const PREV = "page-prev";
+const NEXT = "page-next";
 /**
  * Class Articles Controller
  */
 class Product {
-  ShowList(req, res,logout) {
-    var login=req.session; 
-    if(logout) 
-    {
-      login.username=null;
-      if(req.user != null) req.user=null;
+  async ShowList(req, res, logout) {
+    var login = req.session;
+    if (logout) {
+      login.username = null;
+      if (req.user != null) req.user = null;
     }
-    var fullproduct = db;
     if (req.user != undefined && req.user != null) {
-      login.username= req.user._doc.name;
+      login.username = req.user._doc.name;
     }
-    res.render('viewlistproducts', { data: fullproduct, user: login.username });
+    // Get all product
+    const fullproduct = await product.find({}).limit(prodPerPage);
+    // Count how many products were found
+    const numOfProducts = await product.count({});
+    // Render the page
+    res.render('viewlistproducts', {
+      data: fullproduct,
+      user: login.username,
+      categories: categories,
+      sortOpts: sortOpts,
+      selectedCate: 0,
+      selectedSort: 0,
+      currentPage: 1,
+      pages: Math.ceil(numOfProducts / prodPerPage)
+    });
   }
   async ShowIf(req, res) {
-    var dbif = [];
-    var selectedOpt = req.body.Category;
-    var cateId = await parseToInt(selectedOpt);
-    var dbsession=req.session; 
-    if (cateId == 0) res.render('viewlistproducts', { data: db, user:dbsession.username });
-    else {
-      db.forEach(function (doc) {
-        if (doc.id_category == cateId) dbif.push(doc);
-      });
-      res.render('viewlistproducts', { data: dbif, user:dbsession.username });
+    var query = {};
+    var sort = {};
+    var pageNo = 1;
+    var pageDirect = "";
+    var cateId = await parseToInt(req.body.Category);
+    var sortId = await parseToInt(req.body.SortBy);
+    var dbsession = req.session;
+    // Check if previous or next page was click, then change page number
+    if (req.body.pageNext != undefined && req.body.pageNext != null && req.body.pageNext !== "") {
+      pageNo = await parseToInt(req.body.pageNext);
+      pageDirect = NEXT;
+    } else if (req.body.pagePrev != undefined && req.body.pagePrev != null && req.body.pagePrev !== "") {
+      pageNo = await parseToInt(req.body.pagePrev);
+      pageDirect = PREV;
     }
-
+    // Set query
+    if (cateId !== 0) query = { id_category: cateId };
+    // Set sort order
+    if (sortId === 1) sort = { name: 1 }; // name ascending
+    else if (sortId === 2) sort = { name: -1 }; // name descending
+    else if (sortId === 3) sort = { price: 1 }; // price ascending
+    else if (sortId === 4) sort = { price: -1 }; // price descending
+    // Count how many products were found
+    var dbIf = await product.find(query);
+    const numOfProducts = await product.count(query);
+    // Total page numbers
+    const totalPageNum = Math.ceil(numOfProducts / prodPerPage);
+    // Page number management
+    if (pageDirect === NEXT) {
+      if (pageNo !== totalPageNum) pageNo = pageNo + 1;
+    } else if (pageDirect === PREV) {
+      if (pageNo !== 1) pageNo = pageNo - 1;
+    }
+    // Find filtered product
+    dbIf = await product.find(query)
+      .sort(sort)
+      .limit(prodPerPage)
+      .skip(prodPerPage * (pageNo - 1));
+    // Render page
+    res.render('viewlistproducts', {
+      data: dbIf,
+      user: dbsession.username,
+      categories: categories,
+      sortOpts: sortOpts,
+      selectedCate: cateId,
+      selectedSort: sortId,
+      currentPage: pageNo,
+      pages: totalPageNum
+    });
   }
+  // async Paging(req, res) {
+  //   var query = {};
+  //   var sort = {};
+  //   var cateId = await parseToInt(req.body.Category);
+  //   var sortId = await parseToInt(req.body.SortBy);
+  //   var pageNo = await parseToInt(req.params.page);
+  //   var direct = req.params.direct;
+  //   var dbsession = req.session;
+  //   // Set query
+  //   if (cateId !== 0) query = { id_category: cateId };
+  //   // Set sort order
+  //   if (sortId === 1) sort = { name: 1 }; // name ascending
+  //   else if (sortId === 2) sort = { name: -1 }; // name descending
+  //   else if (sortId === 3) sort = { price: 1 }; // price ascending
+  //   else if (sortId === 4) sort = { price: -1 }; // price descending
+  //   // Check if previous or next page was click, then change page number
+  //   if (direct === PREV) {
+  //     if (pageNo > 0) pageNo = pageNo - 1;
+  //   }
+  //   // Find filtered product
+  //   const dbIf = await product.find(query)
+  //     .sort(sort)
+  //     .limit(prodPerPage)
+  //     .skip(prodPerPage * pageNo);
+  //   // Count how many products were found
+  //   const numOfProducts = await product.count(query);
+  //   // Render page
+  //   res.render('viewlistproducts', {
+  //     data: dbIf,
+  //     user: dbsession.username,
+  //     categories: categories,
+  //     sortOpts: sortOpts,
+  //     selectedCate: cateId,
+  //     selectedSort: sortId,
+  //     currentPage: pageNo + 1,
+  //     pages: Math.ceil(numOfProducts / prodPerPage)
+  //   });
+  // }
   async ShowDetail(req, res) {
     var dbdetail = [];
-    let dbrecommand=[];
+    var dbrecommand = [];
     var idproduct = Number(req.params.id);
-    let cateId;
-    var dbsession=req.session;
-    db.forEach(function (doc) {
-      if (doc._id == idproduct) {
-        dbdetail.push(doc);
-        cateId=doc.id_category;
-       }
+    var dbsession = req.session;
+    // Get product detail
+    dbdetail = await product.find({ _id: idproduct });
+    // Get recommended products
+    dbrecommand = await product.find({
+      id_category: dbdetail.id_category,
+      _id: { $not: { $eq: idproduct } }
     });
-    
-    db.forEach(function(doc){
-      if(doc.id_category==cateId&&doc._id!=idproduct)
-      {
-        dbrecommand.push(doc);
-      }
-    })
-    res.render('product', { data: dbdetail,recommand:dbrecommand,user:dbsession.username });
+    // Render page
+    res.render('product', { data: dbdetail, recommand: dbrecommand, user: dbsession.username });
   }
 
-  async ShowDienthoai(req,res){
-    var dbif = [];
-    //var selectedOpt = 1;
-    //var cateId = await parseToInt(selectedOpt);
-    db.forEach(function(doc){
-      if(doc.id_category==1)
-      {
-        dbif.push(doc);
-      }
-    })
-    res.render('viewtype', { data: dbif});
+  async ShowDienthoai(req, res) {
+    var dbif = product.find({ id_category: 1 });
+    res.render('viewtype', { data: dbif });
   }
 
-  async ShowLaptop(req,res){
-    var dbif = [];
-    //var selectedOpt = 2;
-    //var cateId = await parseToInt(selectedOpt);
-    db.forEach(function(doc){
-      if(doc.id_category==2)
-      {
-        dbif.push(doc);
-      }
-    })
-    res.render('viewtype', { data: dbif});
+  async ShowLaptop(req, res) {
+    var dbif = product.find({ id_category: 2 });
+    res.render('viewtype', { data: dbif });
   }
 
-  async ShowMayanh(req,res){
-    var dbif = [];
-    //var selectedOpt = 1;
-    //var cateId = await parseToInt(selectedOpt);
-    db.forEach(function(doc){
-      if(doc.id_category==6)
-      {
-        dbif.push(doc);
-      }
-    })
-    res.render('viewtype', { data: dbif});
+  async ShowMayanh(req, res) {
+    var dbif = product.find({ id_category: 6 });
+    res.render('viewtype', { data: dbif });
   }
 
-  async ShowTulanh(req,res){
-    var dbif = [];
-    //var selectedOpt = 1;
-    //var cateId = await parseToInt(selectedOpt);
-    db.forEach(function(doc){
-      if(doc.id_category==4)
-      {
-        dbif.push(doc);
-      }
-    })
-    res.render('viewtype', { data: dbif});
+  async ShowTulanh(req, res) {
+    var dbif = product.find({ id_category: 4 });
+    res.render('viewtype', { data: dbif });
   }
 
-  async ShowDongho(req,res){
-    var dbif = [];
-    //var selectedOpt = 1;
-    //var cateId = await parseToInt(selectedOpt);
-    db.forEach(function(doc){
-      if(doc.id_category==5)
-      {
-        dbif.push(doc);
-      }
-    })
-    res.render('viewtype', { data: dbif});
+  async ShowDongho(req, res) {
+    var dbif = product.find({ id_category: 5 });
+    res.render('viewtype', { data: dbif });
   }
 
 }
@@ -127,7 +188,5 @@ function parseToInt(x) {
   }
   return parsed;
 }
-
-
 
 module.exports = Product;
